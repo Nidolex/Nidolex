3d010fe406f995dcac181263d03a9844bfd19ec2
Nidolex <87804872+Nidolex@users.noreply.github.com>
2021-09-23 21:33:20 +0300

    Add files via upload

---
+++ randnums.py
@@ -0,0 +1,16 @@
+from random import randint
+
+def randomizer():
+    numb1 = 1
+    numb2 = 0
+    score = 0
+    while int(numb1) != int(numb2):
+        numb1 = randint(1,100)
+        numb2 = randint(1,100)
+        score += 1
+        print("Первое число: " + str(numb1))
+        print("Второе число: " + str(numb2))
+        if int(numb1) == int(numb2):
+            print("Количество попыток: " + str(score))
+            return
+print(randomizer())
